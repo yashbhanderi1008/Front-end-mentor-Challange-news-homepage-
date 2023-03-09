305773923b6ef82d8b2e6484309bd5bf8dc3731c
yashbhanderi1008 <121347171+yashbhanderi1008@users.noreply.github.com>
2023-03-10 00:00:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    document.querySelector("nav").style.width = "75%";
+    document.querySelector(".overlay").style.display = "block";
+}
+
+function closeMenu() {
+    document.querySelector("nav").style.width = "0%";
+    document.querySelector(".overlay").style.display = "none";
+}
